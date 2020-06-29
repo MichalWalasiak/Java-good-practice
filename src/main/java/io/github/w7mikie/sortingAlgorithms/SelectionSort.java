e6e5ee43ac3cc8bbb58b571dca461527493f2799
Michał Walasiak <m.walasiak@gmail.com>
2020-06-29 13:27:50 +0200

    Implement selection sort algorithm

--- src/main/java/io/github/w7mikie/sortingAlgorithms/SelectionSort.java
+++ src/main/java/io/github/w7mikie/sortingAlgorithms/SelectionSort.java
@@ -3,6 +3,22 @@ package io.github.w7mikie.sortingAlgorithms;
 public class SelectionSort {
     public static void main(String[] args) {
 
+        int[] ints = {4, -5, 45, 1, 4, -74};
+
+        for(int lastUnsortedIndex = ints.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            int largest = 0;
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (ints[i] > ints[largest]){
+                    largest = i;
+                }
+            }
+            swap(ints, largest, lastUnsortedIndex);
+        }
+
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println(ints[i]);
+        }
+
     }
 
     static void swap(int[] array, int i, int j){
