d9790587c7325a58bc275189bf2672c6b9f6ba5d
Michał Walasiak <m.walasiak@gmail.com>
2020-06-30 13:24:04 +0200

    Implement merge method

--- src/main/java/io/github/w7mikie/sortingAlgorithms/MergeSort.java
+++ src/main/java/io/github/w7mikie/sortingAlgorithms/MergeSort.java
@@ -5,4 +5,22 @@ public class MergeSort {
     public static void main(String[] args) {
 
     }
+
+    public static void merge(int[] array, int start, int mid, int end) {
+        if (array[mid - 1] >= array[mid]){
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end - start];
+        while (i < mid && j < end){
+            temp[tempIndex++] = array[i] >= array[j] ? array[i++] : array[j++];
+        }
+
+        System.arraycopy(array, i, array, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, array, start, tempIndex);
+    }
 }
