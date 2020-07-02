2a76b1d6bc9201d4fa3ad3901dcbac670f13eaaf
Michał Walasiak <m.walasiak@gmail.com>
2020-07-02 12:48:03 +0200

    Add radixStringSingleSort method

--- src/main/java/io/github/w7mikie/sortingAlgorithms/RadixStringSort.java
+++ src/main/java/io/github/w7mikie/sortingAlgorithms/RadixStringSort.java
@@ -6,6 +6,28 @@ public class RadixStringSort {
 
     }
 
+    public static void radixStringSingleSort(String[] array, int position, int radix) {
+        int numItems = array.length;
+        int[] countArray = new int[radix];
+
+        for (String value: array){
+            countArray[getIndex(position, value)]++;
+        }
+
+        for(int j = 1; j < radix; j--){
+            countArray[j] += countArray[j - 1];
+        }
+
+        String[] temp = new String[numItems];
+        for (int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--){
+            temp[--countArray[getIndex(position, array[tempIndex])]] = array[tempIndex];
+        }
+
+        for (int tempIndex = 0; tempIndex < numItems; tempIndex++){
+            array[tempIndex] = temp[tempIndex];
+        }
+    }
+
     public static int getIndex(int position, String value) {
         return value.charAt(position) - 'a';
     }
