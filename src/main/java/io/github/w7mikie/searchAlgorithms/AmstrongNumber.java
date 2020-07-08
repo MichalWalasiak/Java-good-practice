0f471b87d7927d5d9067cafc823d4a3552b6ae73
Michał Walasiak <m.walasiak@gmail.com>
2020-07-08 23:54:56 +0200

    Sample detect an Amstrong Number

--- src/main/java/io/github/w7mikie/searchAlgorithms/AmstrongNumber.java
+++ src/main/java/io/github/w7mikie/searchAlgorithms/AmstrongNumber.java
@@ -1,11 +1,29 @@
 package io.github.w7mikie.searchAlgorithms;
 
 
+import java.util.Scanner;
+
 public class AmstrongNumber {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please insert a positive number");
 
+        double z = 0;
+        int x = scanner.nextInt();
+        int y = x;
+
+        while (y != 0){
+            int ams = y % 10;
+            z = z + Math.pow(ams, numberOfIntDigits(x));
+            y = y / 10;
+        }
+        if (z == x){
+            System.out.println(x + " is an Amstrong number");
+        }else{
+            System.out.println(x + " is not an Amstrong number");
+        }
     }
 
 
