34e72966a595e6cd091235ed97e24c4589144150
Michał Walasiak <m.walasiak@gmail.com>
2020-08-04 23:00:51 +0200

    Sample find longest common prefix

---
+++ src/main/java/io/github/w7mikie/string/LongestPrefix.java
@@ -0,0 +1,24 @@
+package io.github.w7mikie.string;
+
+public class LongestPrefix {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public String longestCommonPrefix(String[] args) {
+        if (args.length == 0){
+            return "";
+        }
+        String prefix = args[0];
+        for (int i = 1; i < args.length; i++)
+            while (args[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()){
+                    return "";
+                }
+            }
+        return prefix;
+    }
+}
