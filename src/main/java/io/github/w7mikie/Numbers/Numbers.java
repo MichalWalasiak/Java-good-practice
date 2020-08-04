e148faa9cc4159a99e3e3c3e3ff48347cca4d4e0
Michał Walasiak <m.walasiak@gmail.com>
2020-08-04 18:45:37 +0200

    Sample ways to reverse Integer

--- src/main/java/io/github/w7mikie/Numbers/Numbers.java
+++ src/main/java/io/github/w7mikie/Numbers/Numbers.java
@@ -1,4 +1,30 @@
 package io.github.w7mikie.Numbers;
 
 public class Numbers {
+
+    public static void main(String[] args) {
+        Numbers numbers = new Numbers();
+        System.out.println(numbers.reverseWithString(54874));
+
+
+    }
+    public Integer reverseInteger(Integer input) {
+        Integer reverse = 0;
+
+        while (input != 0) {
+            reverse = reverse * 10 + input % 10;
+            input = input / 10;
+        }
+
+        return reverse;
+    }
+
+    public Integer reverseWithString(Integer input) {
+        String reverse = new StringBuilder().append(Math.abs(input)).reverse().toString();
+        try {
+            return (input < 0) ? Integer.parseInt(reverse) * -1 : Integer.parseInt(reverse);
+        }catch (NumberFormatException e){
+            return 0;
+        }
+    }
 }
