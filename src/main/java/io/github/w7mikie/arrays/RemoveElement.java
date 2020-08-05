cfec4891e2b20a5a01cd7ad7d4d89ad73e83a56c
Michał Walasiak <m.walasiak@gmail.com>
2020-08-05 13:49:19 +0200

    Sample "remove" of element from array

--- src/main/java/io/github/w7mikie/arrays/RemoveElement.java
+++ src/main/java/io/github/w7mikie/arrays/RemoveElement.java
@@ -1,4 +1,21 @@
 package io.github.w7mikie.arrays;
 
 public class RemoveElement {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public int removeElement(int[] input, int val) {
+        int i = 0;
+        for (int j = 0; j < input.length; j++){
+            if (input[j] != val){
+                input[i] = input[j];
+                i++;
+            }
+        }
+        return i;
+
+    }
 }
