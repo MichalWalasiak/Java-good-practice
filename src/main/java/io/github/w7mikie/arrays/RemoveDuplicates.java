ff02d2cf684e3bda5e1128c8ac48718f18d4bd95
Michał Walasiak <m.walasiak@gmail.com>
2020-08-05 13:25:08 +0200

    Sample removing duplicate in array

--- src/main/java/io/github/w7mikie/arrays/RemoveDuplicates.java
+++ src/main/java/io/github/w7mikie/arrays/RemoveDuplicates.java
@@ -1,4 +1,26 @@
 package io.github.w7mikie.arrays;
 
+import java.util.Arrays;
+
 public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public int removeAllDuplicates(int[] input) {
+        if (input.length == 0){
+            return 0;
+        }
+
+        int i = 0;
+        for (int j = 1; j < input.length; j++){
+            if (input[j] != input[i]){
+                i++;
+                input[i] = input[j];
+            }
+        }
+        return i + 1;
+    }
 }
